545413b061cdbabd949ef4e5ec2199bf8f26413f
Alejandro Howez <aech-12@hotmail.com>
2019-03-08 20:55:41 -0300

    'TBFORM'

--- src/components/thirdPage/contact.js
+++ src/components/thirdPage/contact.js
@@ -5,26 +5,29 @@ const Contact = () => (
   <div className='Contact' id='Contact'>
     <div className='contactIn'>
       <h4>Contact me</h4>
-      <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+      <form className='form'
+      method='post' name='contactAHGats' 
+      data-netlify='true' data-netlify-honeypot='bot-field'
+      data-netlify-recaptcha="true">
+        {/* <div data-netlify-recaptcha="true"></div> */}
+        {/* <div className='subject'>
+          <p>Your name</p> */}
+          <input name='subject' type='text'/>
+        {/* </div>
+        <div className='from'>
+          <p>Your email</p>
+          <input name='email' type='email'
+          placeholder='mail@mail.com'/>
+        </div>
+        <div className='yourMessage'>
+          <p>Your message</p>
+          <textarea 
+            name='message' type='text'
+            placeholder={`If you want to work with me or ask about something, feel free to email me`}
+          />
+        </div> */}
+        <button type='submit'>SEND</button>
+      </form>
       <p style={{textAlign: `center`}}>(or email me at aech-12@hotmail.com)</p>
     </div>
   </div>
