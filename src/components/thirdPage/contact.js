f3ab542add464009b010d80ce735a5d7daa92848
Alejandro Howez <aech-12@hotmail.com>
2019-03-08 22:02:52 -0300

    'done?'

--- src/components/thirdPage/contact.js
+++ src/components/thirdPage/contact.js
@@ -6,25 +6,28 @@ const Contact = () => (
     <div className='contactIn'>
       <h4>Contact me</h4>
       
-      <form method="post" action="#" name="contactMe" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="bot-field" />
-    <div className="field half first">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-    </div>
-    <div className="field half">
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
-    </div>
-    <div className="field">
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6"></textarea>
-    </div>
-    <ul className="actions">
-        <li><input type="submit" value="Send Message" className="special" /></li>
-        <li><input type="reset" value="Clear" /></li>
-    </ul>
-</form>
+      <form method="post" action="#" name="contactMe" method="post" 
+      data-netlify="true" data-netlify-honeypot="bot-field"
+      className='form'>
+        <input type="hidden" name="bot-field" />
+        <div className="field half first subject">
+            <label htmlFor="name"><p>Your name</p></label>
+            <input type="text" name="name" id="name" />
+        </div>
+        <div className="field half" className='from'>
+            <label htmlFor="email"><p>Your email</p></label>
+            <input type="text" name="email" id="email" placeholder='mail@mail.com'/>
+        </div>
+        <div className="yourMessage">
+            <label htmlFor="message"><p>Your message</p></label>
+            <textarea name="message" id="message"
+            placeholder={`Want to work with me or ask about something?`}></textarea>
+        </div>
+        <ul className="actions">
+            <li><input type="submit" value="Send Message" className="special button" /></li>
+            {/* <li><input type="reset" value="Clear" /></li> */}
+        </ul>
+      </form>
 
       <p style={{textAlign: `center`}}>(or email me at aech-12@hotmail.com)</p>
     </div>
